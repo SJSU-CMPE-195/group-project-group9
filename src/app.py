94d9261996ffb615114dfc37518276e5377274e2
Aaron2882 <aaron.chen@sjsu.edu>
2026-04-13 11:25:33 -0700

    Rename merged html sidebar to app, remove unused references

--- src/app.py
+++ src/app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/sidebar")
+@app.route("/app")
 def home():
-    return render_template("sidebar.html")
-
-@app.route("/pdfViewer")
-def pdfViewer():
-    return render_template("pdfViewer.html")
+    return render_template("app.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
